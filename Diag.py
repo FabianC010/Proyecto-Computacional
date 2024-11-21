b1e704a49fc9dca8acf06b3745f402d4b2dbec64
FabianC010 <fabian.cortescaravaca@ucr.ac.cr>
2024-11-21 12:36:14 -0600

    (StateQuant0-StateQuant4) to a single StateQuants
    Takes the times.size and psi.size to create a matrix of the corresponding statequants, for ease of access and enables the graphing method previously uploaded.

--- Diag.py
+++ Diag.py
@@ -33,34 +33,27 @@ def exps(eigenValues, h, oldState):
 
 startTime = time.time()                          # Se empieza a tomar el tiempo.
 
-epsilon = np.array([1.0, 2.0, 3.0, 4.0, 5.0])    # Valores energéticos de la diagonal principal.
-t_enr = np.array([1.0, 2.0, 3.0, 4.0])           # Valores energéticos de las diagonales secundarias.
+epsilon = np.full(1000, 10)                       # Valores energéticos de la diagonal principal.
+t_enr = np.full(999, 1)                           # Valores energéticos de las diagonales secundarias.
 H = Hm.crear_Hamil(epsilon, t_enr)               # Se crea el hamiltoniano con los valores enérgeticos asignados.
 
 eigVals, eigVecs = np.linalg.eigh(H)             # np.linalg.eigh consigue los autovalores y 
                                                  # autovectores del hamiltoniano.
 
-psi = np.array([0.0, 0.0, 1.0, 0.0, 0.0])        # Declaramos el vector del estado inicial.
+psi = np.zeros(1000)
+psi[psi.size//2] = 1.0                           # Declaramos el vector del estado inicial.
 psi_t = eigVecs.conjugate().transpose().dot(psi) # Declaramos el vector del estado inicial pero en la nueva base.
                                                  # Para eso multiplicamos la daga de la matriz de autovectores por
                                                  # el estado inicial en la base original.
 
-times = np.linspace(0.0, 10.0, 100, dtype = float)  # Se crea el arreglo de tiempos.
+times = np.linspace(0.0, 30.0, 100, dtype = float)  # Se crea el arreglo de tiempos.
 h = times[1] - times[0]                             # Se calcula el paso temporal que hay entre cada uno de ellos.
 
 #Se declaran los distintos estados
-stateQuant0 = np.zeros(times.size, dtype = object)
-stateQuant1 = np.zeros(times.size, dtype = object)
-stateQuant2 = np.zeros(times.size, dtype = object)
-stateQuant3 = np.zeros(times.size, dtype = object)
-stateQuant4 = np.zeros(times.size, dtype = object)
+stateQuant0 = np.zeros((times.size, psi.size), dtype = complex)
 
 for tt in range(times.size):
-    stateQuant0[tt] = psi[0] 
-    stateQuant1[tt] = psi[1]
-    stateQuant2[tt] = psi[2]
-    stateQuant3[tt] = psi[3]
-    stateQuant4[tt] = psi[4]
+    stateQuant0[tt] = psi 
     psi_t = exps(eigVals, h, psi_t) # Se calcula el nuevo estado pero en la base "diagonal".
     psi = eigVecs.dot(psi_t) # Se convierte a la base original para guardar sus entradas.
 
