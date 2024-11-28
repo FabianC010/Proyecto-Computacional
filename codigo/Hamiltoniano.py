6382620fa01018a97fbdf40ca41653e6a412a82e
Fabricio <f.cesp21@gmail.com>
2024-11-28 17:58:52 -0600

    Creación de la carpeta codigo

--- codigo/Hamiltoniano.py
+++ codigo/Hamiltoniano.py
@@ -8,7 +8,7 @@ def crear_Hamil(epsilon, t_enr):
     Esta función crear un Hamiltoniano con la característica de ser tridiagonal
     con los valores de un "vector" epsilon en la diagonal principal y en las otras
     diagonales los valores de un "vector" t_enr. En caso que los valores no 
-    coincidan con los tamaños de las diagonales se imprime un mensaje de error.
+    coincidan, la función levanta una excepción `ValueError`.
 
     Examples:
         >>> crear_Hamil(np.array([1.0, 2.0, 3.0, 4.0]), np.array([1.0 ,2.0 ,3.0]))
@@ -21,16 +21,19 @@ def crear_Hamil(epsilon, t_enr):
         epsilon (ndarray): Valores energéticos que son asignados a la diagonal principal.
         t_enr (ndarray): Valores energéticos que son asignados a las diagonales secundarias.
 
+    Raises:
+        ValueError: Si t_enr.size no es igual a epsilon.size - 1.
+
     Returns:
-        ndarray: Devuelve el Hamiltoniano tridiagonal si los valores coinciden
+        Hamiltoniano (ndarray): Devuelve el Hamiltoniano tridiagonal si los valores coinciden
     """ 
     Hamiltoniano = np.zeros((epsilon.size, epsilon.size))
 
-    if (t_enr.size == epsilon.size -1):
+    if (t_enr.size == epsilon.size - 1):
         Hamiltoniano += np.diagflat(epsilon) + np.diagflat(t_enr, 1) + np.diagflat(t_enr, -1)
         return Hamiltoniano
 
     else:
-        print("Valores energéticos no coinciden con el tamaño del Hamiltoniano.")
-        return Hamiltoniano
+        raise ValueError ("Valores energéticos no coinciden con el tamaño del Hamiltoniano.")
 
+#print(help(crear_Hamil))
