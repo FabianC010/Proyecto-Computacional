f487777d4d3cac21ec8fc9b65e066484f69294f0
RudyMiranda04 <rudy.mirandacambronero@ucr.ac.cr>
2024-12-03 14:23:47 -0600

    __init__.py 3.0

--- __init__.py
+++ __init__.py
@@ -8,8 +8,7 @@ Módulos exportados por este paquete:
 -`dyn_Generator: Generador de la dinámica de un sistema.`
 -`rk4: Implementación del método de Runge-Kutta de orden 4.`
 -`norma_cuadrada: Cálculo de la norma al cuadrado de cada componente de un vector.`
--`inicializacion: Crea los parámetros iniciales de un sistema de Tight Binding con un fermión en medio de la grilla. Esta función genera los parámetros iniciales del sistema en el cual, el Hamiltoniano contiene valores
-    energéticos homogéneos. Todos los epsilon_i tienen el mismo valor y los t_i tienen el mismo valor ente sí.`
+-`inicializacion: Crea los parámetros iniciales de un sistema de Tight Binding con un fermión en medio de la grilla.`
 -`simulacion_Rk: Simulación de la evolución temporal de un sistema de Tight Binding utilizando el método de RK4. Se requiere utilizar con inicializacion.`
 -`simulacion_Diag: Simulación de la evolución temporal de un sistema de Tight Binding utilizando la diagonalización del Hamiltoniano. Se requiere utilizar con inicialización`
 """
